e0ab04f78b1e10f767c5c6738b345ea3ccea99bc
whdwns268 <munjongjun@gmail.com>
2024-03-19 07:22:15 +0900

    port 3000 -> 8000

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const formDataGet = require('./formDataGet');
 
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.use(async (req, res, next) => {
     if (!browser || !page) {
